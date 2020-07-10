d070b7a24e5444d1d20ba9829b64aa2fd8f888cb
pranjalikawale <pranjalikawale1578@gmail.com>
2020-07-10 10:37:19 +0530

    ADDED:Test for class locator and drop down

--- test_for_locator_and_drop_down.py
+++ test_for_locator_and_drop_down.py
@@ -56,7 +56,7 @@ class TestLocator():
         time.sleep(2)
         assert driver.title=="Find a Flight: Mercury Tours:"
         
-         def test_for_id_locator_and_waits(self,register_driver):
+    def test_for_id_locator_and_waits(self,register_driver):
         driver.implicitly_wait(5)
         driver.maximize_window()
         url="http://demo.guru99.com/test/facebook.html"
@@ -72,6 +72,41 @@ class TestLocator():
         wait=WebDriverWait(driver,5)
         status=wait.until(EC.title_is(u"Broward College - Fort Lauderdale, FL - Organization, College & University | Facebook"))
         assert status==True
+
+    def test_for_class_locator_and_drop_down(self,register_driver):
+        driver.implicitly_wait(5)
+        driver.maximize_window()
+        url="https://fs2.formsite.com/meherpavan/form2/index.html?1537702596407"
+        driver.get(url)
+
+        #input text field
+        inputboxes=driver.find_elements(By.CLASS_NAME,"text_field")
+        print(len(inputboxes))
+        driver.find_element(By.ID,"RESULT_TextField-1").send_keys("pranjali")
+        driver.find_element(By.ID,"RESULT_TextField-2").send_keys("kawale")
+
+        #drop down
+        drop_down=Select(driver.find_element_by_id("RESULT_RadioButton-9"))
+
+        #select by visible text
+        drop_down.select_by_visible_text('Morning')
+
+        #select by index
+        drop_down.select_by_index(2)
+
+        #select by index
+        drop_down.select_by_value("Radio-2")
+
+        #count of drop down option
+        print(len(drop_down.options))
+
+        #Capture all option
+        all_options=drop_down.options
+
+        for option in all_options:
+            print(option.text)
+
+        assert driver.current_url==url
    
 
 
